df43cf5fef073e88984386cac176f5db35fa1901
KEG <kory.gauger@ufl.edu>
2023-02-03 17:58:01 -0500

    Added main.go as a start

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("This is a start")
+
+	// need to open a file and save it in the program in some way
+	// should probably use a struct and edit it using functions
+}

379d2bfe458228409fe4c499c0a4b2f08a39edf6
AmariN12 <101675831+AmariN12@users.noreply.github.com>
2023-02-03 14:24:45 -0500

    Create User Stories.m
    A collection of our user stories.

---
+++ User Stories.m
@@ -0,0 +1,9 @@
+# User Stories
+
+As a university student, I want to be able to bold, underline, and italicize my text so I can emphasize important points in my notes!
+
+As a club leader, I want to be able to create bullet points and align text so I can organize the club's information in a neat way!
+
+As Greg Heffley, I want to be able to use different size texts and fonts to make my dairy feel personal. I also like my privacy, so a log-in page will make me feel secure.
+
+As a power user, I want to have shortcuts to speed up the note taking process.

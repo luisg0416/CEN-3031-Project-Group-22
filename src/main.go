76c2d7e3f7133f4b0996b3c918f6cc99d0007504
KEG <kory.gauger@ufl.edu>
2023-02-06 18:31:49 -0500

    Added a struct to save the lines of the document to

--- src/main.go
+++ src/main.go
@@ -5,21 +5,31 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"log"
 )
 
-func ReadFile(fileName string) {
+type Page struct {
+	lines []string
+}
+
+func ReadFile(fileName string) Page {
+	
+	newPage := Page{}
+	
 	file, err := os.Open(fileName)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return newPage
 	}
-
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())  // where we get the info line by line
+		//fmt.Println(scanner.Text())
+		newPage.lines = append(newPage.lines, scanner.Text())
 	}
+
+	return newPage
 }
 
 // this will be to read the config file to find where our notes are stored and any other configs we need to save
@@ -43,7 +53,19 @@ func ReadConfig(fileName string) {
 func main() {
 
 	// need to open a file and save it in the program in some way
-	ReadFile("testNote.txt")
+	path, err := os.Getwd()
+
+    if err != nil {
+        log.Println(err)
+    }
+
+    fmt.Println(path)
+
+	newPage := ReadFile("testNote.txt")
+
+	for i, element := range newPage.lines {
+		fmt.Println(i, element)
+	}
 	
 
 	// should probably use a struct and edit it using functions

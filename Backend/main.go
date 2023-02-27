6b02fbc6c0d5acd6ab0c7581b5864dc9f72ed7a9
KEG <kory.gauger@ufl.edu>
2023-02-27 00:11:15 -0500

    Start to the REST api using fiber

--- Backend/main.go
+++ Backend/main.go
@@ -1,96 +1,40 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
 )
 
-type Page struct {
-	lines []string
-}
-
-func ReadFile(fileName string) Page {
-	
-	newPage := Page{}
-	
-	file, err := os.Open(fileName)
-	if err != nil {
-		fmt.Println(err)
-		return newPage
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		//fmt.Println(scanner.Text())
-		newPage.lines = append(newPage.lines, scanner.Text())
-	}
-
-	return newPage
-}
-
-// this will be to read the config file to find where our notes are stored and any other configs we need to save
-func ReadConfig(fileName string) {
-	file, err := os.Open(fileName)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		keyVal := strings.Split(scanner.Text(), "=")
-		fmt.Println(keyVal[0])
-		fmt.Println(keyVal[1])
-	}
-}
-
-func (p *Page) SaveNewPage(filePath string) {
-	file, err := os.Create(filePath)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	defer file.Close()
-
-	for _,s := range p.lines {
-		_, writeErr := file.WriteString(s)
-		if writeErr != nil {
-			fmt.Println(writeErr)
-		}
-		file.WriteString("\n")
-	}
+type card struct {
+	id         int    `json:"id"`
+	word       string `json:"word"`
+	definition string `json:"definition"`
 }
 
 func main() {
 
-	// need to open a file and save it in the program in some way
+	app := fiber.New()
 
-	reader := bufio.NewReader(os.Stdin)
+	flashCards := []card{}
 
-	fmt.Println("Enter File to Open:")
-	fileName,_ := reader.ReadString('\n')
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
 
-	fileName = strings.TrimSuffix(fileName, "\n")
-	fileName = strings.TrimSuffix(fileName, "\r")
+	app.Post("/api/flashCards", func(c *fiber.Ctx) error {
+		flashCard := &card{}
 
-	newPage := ReadFile(fileName)
+		if err := c.BodyParser(flashCard); err != nil {
+			return err
+		}
 
-	for i, element := range newPage.lines {
-		fmt.Println(i, element)
-	}
+		flashCard.id = len(flashCards) + 1
 
-	fmt.Println("Enter File to Save to:")
-	fileName2,_ := reader.ReadString('\n')
+		flashCards = append(flashCards, *flashCard)
 
-	fileName2 = strings.TrimSuffix(fileName2, "\n")
-	fileName2 = strings.TrimSuffix(fileName2, "\r")
+		return c.JSON(flashCard)
+	})
 
-	newPage.SaveNewPage(fileName2)
-	
-
-	// should probably use a struct and edit it using functions
+	log.Fatal(app.Listen(":3000"))
 }

76e086790069cc489ae316b424411de24ed20723
Luis Guerrero <guerrerol@ufl.edu>
2023-02-28 19:33:41 -0500

    added some pages

---
+++ React Project/study-website/src/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>Fea</h1>
+            <div className="flashcards"></div>
+            <div className="notes"></div>
+            <div className="games"></div>
+        </div>
+    );
+}
+
+export default Home

0bde477e55089d23792f40961db2efcf42a61542
KEG <kory.gauger@ufl.edu>
2023-03-01 19:25:45 -0500

    "stuff and things

--- Routes/routes.go
+++ Routes/routes.go
@@ -1,5 +1,20 @@
 package routes
 
-func routes() {
+import "github.com/gofiber/fiber/v2"
+
+func Routes(app *fiber.App) {
 	
+	app.Get("/api", func(c *fiber.Ctx) error {
+		return c.SendString("golang api up and running")
+	})
+
+	app.Post("/api/flashCards", CreateFlashCard)
+
+	app.Get("/api/flashCards", GetFlashCards)
+
+	app.Get("/api/flashCards/:id", GetFlashCardsID)
+
+	// app.Get("*", func(c *fiber.Ctx) error {
+	// 	return c.Render("index", fiber.Map{})
+	// })
 }
--- main.go
+++ main.go
@@ -6,61 +6,19 @@ import (
 	"fmt"
 	"log"
 	"github.com/gofiber/fiber/v2"
-	"github.com/luisg0416/CEN-3031-Project-Group-22/database"
-	"github.com/luisg0416/CEN-3031-Project-Group-22/Models"
-	//"github.com/luisg0416/CEN-3031-Project-Group-22/Routes"
+	//"github.com/luisg0416/CEN-3031-Project-Group-22/database"
+	"github.com/luisg0416/CEN-3031-Project-Group-22/Routes"
 )
 
-
-var flashCards []Models.Card
 
 func main() {
 
 	fmt.Println("this is just so vscode shuts up about my imports")
 	
-	database.ConnectDb()
+	//database.ConnectDb()
 	app := fiber.New()
 
-	app.Get("/api", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
-
-	app.Post("/api/flashCards", func(c *fiber.Ctx) error {
-		flashCard := &Models.Card{}
-	
-		if err := c.BodyParser(flashCard); err != nil {
-			return err
-		}
-	
-		flashCard.Id = len(flashCards) + 1
-	
-		flashCards = append(flashCards, *flashCard)
-	
-		//fmt.Println(flashCard)
-	
-		return c.JSON(flashCards)
-	})
-
-	app.Get("/api/flashCards", func(c *fiber.Ctx) error {
-		return c.JSON(flashCards)
-	})
-
-	app.Get("/api/flashCards/:id", func(c *fiber.Ctx) error {
-		
-		id, err := c.ParamsInt("id")
-		if err != nil {
-			return c.Status(401).SendString("Invalid id")
-		}
-
-		var card Models.Card
-		for _, card := range flashCards {
-			if card.Id == id {
-				return c.JSON(card)
-			}
-		}
-
-		return c.JSON(card)
-	})
+	routes.Routes(app)
 
 	log.Fatal(app.Listen(":3000"))
 }

5b4f391edfbd03be2bfdb0293a5b0e429e4fdea1
Luis Guerrero <80639597+luisg0416@users.noreply.github.com>
2023-04-19 22:50:49 -0400

    Update Flashcards.js

--- frontend/pages/Flashcards.js
+++ frontend/pages/Flashcards.js
@@ -2,8 +2,8 @@ import React, {useState} from "react";
 import { Typography, Button, IconButton, AppBar, Card, Box, CardActions, CardActionArea, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material'
 import FlashcardList from "../FlashcardList";
 
-function Flashcards () {
-    const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+function Flashcards ({props}) {
+    const [flashcards, setFlashcards] = useState(props)
     return (
         <div className="home">
             <CssBaseline/>
@@ -11,41 +11,4 @@ function Flashcards () {
         </div>
     );
 }
-
-const SAMPLE_FLASHCARDS = [
-    {
-        id: 1,
-        question: 'What is 2 + 2?' ,
-        answer: '4',
-        options: [
-            '2',
-            '3',
-            '4',
-            '5',
-        ]
-    },
-    {
-        id: 2,
-        question: 'What is 3 + 3?' ,
-        answer: '6',
-        options: [
-            '4',
-            '5',
-            '6',
-            '7',
-        ]
-    },
-    {
-        id: 3,
-        question: 'What is 4 + 4?' ,
-        answer: '8',
-        options: [
-            '5',
-            '6',
-            '7',
-            '8',
-        ]
-    }
-]
-
 export default Flashcards

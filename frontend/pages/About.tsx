67071f7dfb45c86e591faed5929e397e4ffa6d5e
Luis Guerrero <80639597+luisg0416@users.noreply.github.com>
2023-04-19 22:50:21 -0400

    Update About.tsx

--- frontend/pages/About.tsx
+++ frontend/pages/About.tsx
@@ -6,6 +6,7 @@ export default function About() {
     <div className="about">
       <CssBaseline/>
       <h1>About</h1>
+      <h2>The goal of this website is to help those in need better prepare for their classes</h2>
     </div>
   );
 }
